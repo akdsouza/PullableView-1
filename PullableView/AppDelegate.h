0382871810d260c78d62c0a5ba91d105d81aaf0f
Ahmed Henawey <henawey@gmail.com>
2014-12-19 06:17:48 +0200

    Initial Commit

---
+++ PullableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PullableView
+//
+//  Created by Ahmed Henawey on 19/12/2014.
+//  Copyright (c) 2014 Ahmed Henawey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PullableView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PullableView
+//
+//  Created by Ahmed Henawey on 19/12/2014.
+//  Copyright (c) 2014 Ahmed Henawey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
